b1ed9796ad498ffe7c3ecf11b7478b8c1f39e250
Kanowie <n.abdo613@gmail.com>
2021-02-25 13:53:23 -0500

    removed those silly class tags in the header and nav

--- resources/css/claws.js
+++ resources/css/claws.js
@@ -1,3 +1,7 @@
 let bodyElement = document.querySelector('body');
 bodyElement.style.backgroundColor = 'white';
 
+let navBar = document.querySelector('nav');
+navBar.style.display = 'inline';
+
+let headerBan = document.querySelector('header');
