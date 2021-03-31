420dc0a0f6cb914eafa1ec2452246f53b0b3b53e
Kanowie <n.abdo613@gmail.com>
2021-03-31 19:53:00 -0400

    added text and transitioned the artists image. Must make text circular with JS

--- resources/css/claws.js
+++ resources/css/claws.js
@@ -1,7 +1,18 @@
 let bodyElement = document.querySelector('body');
 bodyElement.style.backgroundColor = 'white';
 
-let navBar = document.querySelector('nav');
-navBar.style.display = 'inline';
+/*curving the letters over the image*/
+function circularText = (txt, radius, index) => {
+    txt = txt.split(""),
+    document.getElementById("curved")[classIndex];
 
-let headerBan = document.querySelector('header');
+    var deg = 360 / txt.length,
+    origin = 0;
+
+    txt.forEach((ea) => {
+        ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+        classIndex.innerHTML += ea;
+        origin += deg;
+    });
+}
+circularText("Thee Artisit" 100, 0);
