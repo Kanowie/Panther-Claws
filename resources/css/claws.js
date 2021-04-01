87c96baa61cb3f243a9aaadd9713365cbb862d05
Kanowie <n.abdo613@gmail.com>
2021-04-01 18:59:16 -0400

    added the ciruclar text over Josies image with Javascript

--- resources/css/claws.js
+++ resources/css/claws.js
@@ -2,17 +2,33 @@ let bodyElement = document.querySelector('body');
 bodyElement.style.backgroundColor = 'white';
 
 /*curving the letters over the image*/
-function circularText = (txt, radius, index) => {
-    txt = txt.split(""),
-    document.getElementById("curved")[classIndex];
+const angleToRadian = (angle) => {
+    return angle * (Math.PI / 180);
+};
+const radius = 150;
+const diameter = radius * 2;
 
-    var deg = 360 / txt.length,
-    origin = 0;
+const circle = document.querySelector('#circText');
+circle.style.width = `${diameter}px`;
+circle.style.height = `${diameter}px`;
 
-    txt.forEach((ea) => {
-        ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
-        classIndex.innerHTML += ea;
-        origin += deg;
-    });
-}
-circularText("Thee Artisit" 100, 0);
+const text = circle.innerText;
+const characters = text.split("");
+circle.innerText = null;
+
+let angle = -90;
+const deltaAngle = 360 / characters.length;
+
+characters.forEach((char, index) => {
+    const charElement = document.createElement("span");
+    charElement.innerText = char;
+    const xPos = radius * (1 + Math.cos(angleToRadian(angle)));
+    const yPos = radius * (1 + Math.sin(angleToRadian(angle)));
+
+    const transform = `translate(${xPos}px, ${yPos}px)`;
+    const rotate = `rotate(${index * deltaAngle}deg)`;
+    charElement.style.transform = `${transform} ${rotate}`;
+
+    angle += deltaAngle;
+    circle.appendChild(charElement);
+});
