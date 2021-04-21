5f233d9056649e56ad12e58dd917d2c3a7f86b70
Kanowie <n.abdo613@gmail.com>
2021-04-20 22:57:49 -0400

    added a lot of linear gradient in there for some ziest

--- resources/css/claws.js
+++ resources/css/claws.js
@@ -1,31 +1,8 @@
-/*curving the letters over the image*/
-const angleToRadian = (angle) => {
-    return angle * (Math.PI / 180);
-};
-const radius = 200;
-const diameter = radius * 2;
+const wholeBody = document.querySelector("body");
+wholeBody.style.display.background = "linear-gradient(250deg, rgb(107, 107, 107),rgb(52, 53, 53))";
 
-const circle = document.querySelector('#circText');
-circle.style.width = `${diameter}px`;
-circle.style.height = `${diameter}px`;
+const wholeHeader = document.getElementById("cont-header");
+wholeHeader.style.background ="linear-gradient(175deg, rgb(248, 174, 211), rgb(43, 43, 43))";
 
-const text = circle.innerText;
-const characters = text.split("");
-circle.innerText = null;
-
-let angle = -90;
-const deltaAngle = 360 / characters.length;
-
-characters.forEach((char, index) => {
-    const charElement = document.createElement("span");
-    charElement.innerText = char;
-    const xPos = radius * (1 + Math.cos(angleToRadian(angle)));
-    const yPos = radius * (1 + Math.sin(angleToRadian(angle)));
-
-    const transform = `translate(${xPos}px, ${yPos}px)`;
-    const rotate = `rotate(${index * deltaAngle}deg)`;
-    charElement.style.transform = `${transform} ${rotate}`;
-
-    angle += deltaAngle;
-    circle.appendChild(charElement);
-});
+const wholeNav = document.getElementById("nami-nav");
+document.removeChild("shirts");
