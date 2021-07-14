0af0da60298bba95f862151be30db1bed942e30d
Kanowie <n.abdo613@gmail.com>
2021-07-14 16:23:08 -0400

    making the site more responsive to mobile devices

--- resources/css/claws.js
+++
@@ -1,8 +0,0 @@
-const wholeBody = document.querySelector("body");
-wholeBody.style.display.background = "linear-gradient(250deg, rgb(107, 107, 107),rgb(52, 53, 53))";
-
-const wholeHeader = document.getElementById("cont-header");
-wholeHeader.style.background ="linear-gradient(175deg, rgb(248, 174, 211), rgb(43, 43, 43))";
-
-const wholeNav = document.getElementById("nami-nav");
-document.removeChild("shirts");
